af360ecf4f74b2ff8902fdf5c05832b23fc792cd
Georgiy Filippov <g.filippov777@gmail.com>
2022-05-14 23:40:43 +0500

    feat: add pages

---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const SearchPage = () => {
+  return (
+    <Container>
+      <h1>Search</h1>
+    </Container>
+  )
+}
+
+export default SearchPage
