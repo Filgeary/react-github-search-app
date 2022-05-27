125c988d8339b372758da5b9f183fccf87ccebef
Georgiy Filippov <g.filippov777@gmail.com>
2022-05-28 03:05:28 +0500

    feat: add Search

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import Search from '../components/Search'
 
 const HomePage = () => {
   return (
     <Container>
-      <h1>Home</h1>
+      <h1>Search Github Users & Repos</h1>
+
+      <Search />
     </Container>
   )
 }
