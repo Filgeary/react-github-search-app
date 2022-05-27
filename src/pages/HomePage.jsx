af6f75638e58155f1cfcc90f16df1919dce6abfe
Georgiy Filippov <g.filippov777@gmail.com>
2022-05-28 04:36:33 +0500

    feat: add CardComponent

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Stack } from 'react-bootstrap'
 import Search from '../components/Search'
+import CardComponent from '../components/CardComponent'
 
 const HomePage = () => {
   return (
     <Container>
-      <h1>Search Github Users & Repos</h1>
+      <Stack gap={4}>
+        <h1>Search Github Users & Repos</h1>
 
-      <Search />
+        <Search />
+        <CardComponent />
+      </Stack>
     </Container>
   )
 }
