2e5ccd5eb33d998206173376a4e3adf538c58133
Georgiy Filippov <g.filippov777@gmail.com>
2022-06-20 00:24:56 +0500

    feat: add content to AboutPage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, ListGroup, NavLink, Stack } from 'react-bootstrap'
+
+const dataList = [
+  'React, Hooks',
+  'React-Router',
+  'React-Bootstrap',
+  'React Query',
+  'axios',
+  'lodash (debounce)',
+  'Github API',
+]
 
 const AboutPage = () => {
   return (
     <Container>
-      <h1>About</h1>
+      <h1 className='visually-hidden'>About</h1>
+
+      <Stack gap={5}>
+        <Container>
+          <h2>Tech Stack</h2>
+
+          <ListGroup variant='flush'>
+            {dataList &&
+              dataList.map((item, idx) => (
+                <ListGroup.Item key={idx}>{item}</ListGroup.Item>
+              ))}
+          </ListGroup>
+        </Container>
+
+        <p>
+          Developed by:{' '}
+          <NavLink
+            href='https://github.com/Filgeary'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='d-inline-block px-0 border-bottom'
+          >
+            Filgeary 👉
+          </NavLink>
+        </p>
+      </Stack>
     </Container>
   )
 }
