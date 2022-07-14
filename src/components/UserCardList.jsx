079f44718aab8a481d16e067865dda8e8b58d219
Georgiy Filippov <g.filippov777@gmail.com>
2022-07-14 09:34:57 +0500

    feat: add fadeUp animation to UserCardList.jsx

--- src/components/UserCardList.jsx
+++ src/components/UserCardList.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Form, Row } from 'react-bootstrap'
+import React, { useEffect, useRef, useState } from 'react'
+import { Form, Row, Stack } from 'react-bootstrap'
+import { CSSTransition } from 'react-transition-group'
 import UserCard from './UserCard'
 
 export const UserCardList = ({
@@ -8,37 +9,55 @@ export const UserCardList = ({
   sortFilter,
   isMobile,
 }) => {
+  let stackRef = useRef(null)
+  const [isShow, setIsShow] = useState(false)
+  useEffect(() => {
+    setIsShow(!!userList.items.length)
+  }, [userList])
+
   return (
-    <>
-      <div className='d-flex justify-content-between align-items-center'>
-        <h2 className='mb-0'>Users</h2>
-
-        <Form.Select
-          className='w-auto'
-          onChange={onChangeSelect}
-          defaultValue={sortFilter}
-        >
-          <option value=''>Best Match</option>
-          <option value='followers'>Followers</option>
-          <option value='repositories'>Repos</option>
-        </Form.Select>
-      </div>
-
-      <Row
-        xs={2}
-        md={4}
-        xl={5}
-        className='g-2 justify-content-center g-md-4'
+    <CSSTransition
+      in={isShow}
+      timeout={700}
+      classNames='fadeUp'
+      mountOnEnter
+      unmountOnExit
+      nodeRef={stackRef}
+    >
+      <Stack
+        gap={4}
+        ref={stackRef}
       >
-        {userList &&
-          userList.items?.map(user => (
-            <UserCard
-              key={user.id}
-              user={user}
-              isMobile={isMobile}
-            />
-          ))}
-      </Row>
-    </>
+        <div className='d-flex justify-content-between align-items-center'>
+          <h2 className='mb-0'>Users</h2>
+
+          <Form.Select
+            className='w-auto'
+            onChange={onChangeSelect}
+            defaultValue={sortFilter}
+          >
+            <option value=''>Best Match</option>
+            <option value='followers'>Followers</option>
+            <option value='repositories'>Repos</option>
+          </Form.Select>
+        </div>
+
+        <Row
+          xs={2}
+          md={4}
+          xl={5}
+          className='g-2 justify-content-center g-md-4'
+        >
+          {userList &&
+            userList.items?.map(user => (
+              <UserCard
+                key={user.id}
+                user={user}
+                isMobile={isMobile}
+              />
+            ))}
+        </Row>
+      </Stack>
+    </CSSTransition>
   )
 }
