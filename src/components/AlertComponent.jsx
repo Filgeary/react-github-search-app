a8b0cfd131d36a71ba330bfd8d34187979d375d8
Georgiy Filippov <g.filippov777@gmail.com>
2022-07-14 05:27:42 +0500

    fix: replace heading, add new prop to AlertComponent.jsx

--- src/components/AlertComponent.jsx
+++ src/components/AlertComponent.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Alert } from 'react-bootstrap'
 
-const AlertComponent = ({ variant, heading, text }) => {
+const AlertComponent = ({ variant, heading, text, isDismissible = true }) => {
   const [isShow, setIsShow] = useState(true)
 
   return (
@@ -10,7 +10,7 @@ const AlertComponent = ({ variant, heading, text }) => {
         className='mb-0 px-2 px-md-3'
         variant={variant}
         onClose={() => setIsShow(false)}
-        dismissible
+        dismissible={isDismissible}
         transition
       >
         <Alert.Heading>{heading}</Alert.Heading>
