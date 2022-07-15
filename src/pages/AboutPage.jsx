c1925a15277dfd5beb0f68566df34d1cd677dbe7
Georgiy Filippov <g.filippov777@gmail.com>
2022-07-15 12:41:31 +0500

    refactor: divide AboutPage on 2 components, add constant for dataList

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,68 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Container, ListGroup, NavLink, Stack } from 'react-bootstrap'
-import { CSSTransition } from 'react-transition-group'
-
-const dataList = [
-  'React, Hooks',
-  'React-Router',
-  'React-Bootstrap',
-  'animations (React Transition Group)',
-  'React Query',
-  'axios',
-  'lodash (debounce)',
-  'Github API',
-]
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import About from '../components/About'
+import { TECH_STACK_AND_TOOLS_LIST } from '../constants'
 
 const AboutPage = () => {
-  let contRef = useRef(null)
-  const [isShow, setIsShow] = useState(false)
-  useEffect(() => {
-    setIsShow(true)
-  }, [])
-
   return (
-    <CSSTransition
-      nodeRef={contRef}
-      in={isShow}
-      timeout={1500}
-      classNames='fadeSlowDown'
-      mountOnEnter
-      unmountOnExit
-    >
-      <Container ref={contRef}>
-        <h1 className='visually-hidden'>About</h1>
+    <Container>
+      <h1 className='visually-hidden'>About</h1>
 
-        <Stack gap={5}>
-          <Container>
-            <h2>Tech Stack & tools</h2>
-
-            <ListGroup variant='flush'>
-              {dataList &&
-                dataList.map((item, idx) => (
-                  <ListGroup.Item
-                    key={idx}
-                    style={{ backgroundColor: 'inherit' }}
-                  >
-                    {item}
-                  </ListGroup.Item>
-                ))}
-            </ListGroup>
-          </Container>
-
-          <p>
-            Developed by:{' '}
-            <NavLink
-              href='https://github.com/Filgeary'
-              target='_blank'
-              rel='noopener noreferrer'
-              className='d-inline-block px-0 border-bottom'
-            >
-              Filgeary 👉
-            </NavLink>
-          </p>
-        </Stack>
-      </Container>
-    </CSSTransition>
+      <About dataList={TECH_STACK_AND_TOOLS_LIST} />
+    </Container>
   )
 }
 
