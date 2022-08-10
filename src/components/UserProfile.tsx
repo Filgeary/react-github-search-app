f38e7fa757e1a125873bf9abdb5fcdfa1b23d2a8
Georgiy Filippov <g.filippov777@gmail.com>
2022-08-10 15:09:07 +0500

    feat: add component icons instead of svg

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -11,6 +11,7 @@ import {
   Row,
   Stack,
 } from 'react-bootstrap'
+import { GeoAltFill, Twitter } from 'react-bootstrap-icons'
 import { CSSTransition } from 'react-transition-group'
 import { IUser } from '../models/IUser'
 import { formatTwitterUrl, formatWebsiteUrl } from '../utils'
@@ -75,19 +76,13 @@ const UserProfile: FC<Props> = ({ user }) => {
               <h4>@{login}</h4>
 
               {location && (
-                <p>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='16'
-                    height='16'
-                    fill='currentColor'
-                    className='bi bi-geo-alt-fill'
-                    viewBox='0 0 16 16'
-                  >
-                    <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
-                  </svg>{' '}
+                <div>
+                  <span>
+                    {/*@ts-ignore-next-line*/}
+                    <GeoAltFill />
+                  </span>{' '}
                   {location}
-                </p>
+                </div>
               )}
             </Col>
 
@@ -119,16 +114,8 @@ const UserProfile: FC<Props> = ({ user }) => {
                   )}
                   {twitter_username && (
                     <ListGroupItem className='d-flex align-items-center gap-2 px-0'>
-                      <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        width='16'
-                        height='16'
-                        fill='currentColor'
-                        className='bi bi-twitter'
-                        viewBox='0 0 16 16'
-                      >
-                        <path d='M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z' />
-                      </svg>
+                      {/*@ts-ignore-next-line*/}
+                      <Twitter />
                       <NavLink
                         href={formatTwitterUrl(twitter_username)}
                         target='_blank'
